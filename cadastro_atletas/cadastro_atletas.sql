2c38e8875dd73892f76b9b2905a45043d6273d44
Julio Abreu CanalDev33 <canaldev33@gmail.com>
2025-01-23 12:51:27 -0300

    ajustes no arquivo cadastro_atletas.sql

--- cadastro_atletas/cadastro_atletas.sql
+++ cadastro_atletas/cadastro_atletas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 10/01/2025 às 19:08
+-- Tempo de geração: 23/01/2025 às 12:47
 -- Versão do servidor: 10.6.18-MariaDB-0ubuntu0.22.04.1
 -- Versão do PHP: 8.1.2-1ubuntu2.20
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cadastro_atletas` (
   `id` int(11) NOT NULL,
+  `foto` varchar(100) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `rg` varchar(15) NOT NULL,
   `cpf` varchar(15) NOT NULL,
@@ -48,12 +49,9 @@ CREATE TABLE `cadastro_atletas` (
 -- Despejando dados para a tabela `cadastro_atletas`
 --
 
-INSERT INTO `cadastro_atletas` (`id`, `nome`, `rg`, `cpf`, `endereco`, `numero`, `cep`, `municipio`, `cidade`, `data_nascimento`, `email`, `senha`, `telefone`, `data_cadastro`) VALUES
-(1, 'julio cesar de abreu', '35.234.790-9', '31183817878', 'av antonio bardella', '17', '06618000', 'jandira', 'sao paulo', '1983-02-03', 'juliocasp38@gmail.com', '', '(11) 94647-6117', '2024-12-23 14:48:07'),
-(9, 'Antonio da Silva', '234569879', '31188817878', 'av antonio bardella', '2000', '06619000', 'jandira', 'sao paulo', '2000-10-12', 'antoniodasilva@gmail.com', '', '(11) 94646-6101', '2024-12-27 22:32:36'),
-(10, 'samuel abreu', '123456789', '98765432101', 'av brasil', '1200', '07718000', 'sorocaba', 'são paulo', '1986-04-21', 'samuel@gmail.com', '', '(11) 94613-7985', '2024-12-29 18:50:52'),
-(11, 'josefina da Silva', '35236456', '31165498721', '', '', '', '', '', '2000-10-12', 'josefina@gmail.com', '', '(11) 94646-7979', '2025-01-01 04:38:58'),
-(12, 'joana fagundes', '98987654', '32265478989', 'av nazare', '4500', '06618000', 'itapevi', 'sao paulo', '1990-02-10', 'juliocasp38@gmail.com', '$2y$10$0vFoV1N5BB15FBtvWSmsYu2pHKISI0Edo8g6jHw2Y/fSblLarwDX2', '11946456118', '2025-01-05 01:52:42');
+INSERT INTO `cadastro_atletas` (`id`, `foto`, `nome`, `rg`, `cpf`, `endereco`, `numero`, `cep`, `municipio`, `cidade`, `data_nascimento`, `email`, `senha`, `telefone`, `data_cadastro`) VALUES
+(18, 'arquivos/67922595c2c6f.jpeg', 'antonio velasque', '123564454', '32265478989', 'rua brasil', '2100', '06618000', 'itapevi', 'sao paulo', '1990-02-10', 'canaldev33@gmail.com', '$2y$10$nZaKJLeA9pcfSourxdspR.aNLlMLicfUTOJAD7OpJlhMVpjfFQYpC', '11946476117', '2025-01-23 08:18:45'),
+(19, 'arquivos/679225dd453a1.png', 'marcos antonio velasques', '33234790', '32265498712', 'av nazare', '4500', '06618000', 'Jandira', 'Sao paulo', '1990-02-10', 'canaldev33@gmail.com', '$2y$10$alaUbc32WrPhfRtMVmsgneHxGMVQ5R1FC5y6nQ5qLrGGQ20036dg.', '11946476117', '2025-01-23 08:19:57');
 
 --
 -- Índices para tabelas despejadas
@@ -73,7 +71,7 @@ ALTER TABLE `cadastro_atletas`
 -- AUTO_INCREMENT de tabela `cadastro_atletas`
 --
 ALTER TABLE `cadastro_atletas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
